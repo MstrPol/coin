2398e969d1ac7c596b87eb68f6d618990300932e
nikita polenok <mstrpol@yandex.ru>
2026-07-21 10:24:32 +0700

    chore(coin-executor): удалить устаревшие файлы и конфигурации
    Удалить ненужные файлы, включая .dockerignore, buildkitd.toml, CHARTER.md, Jenkinsfile и другие, связанные с coin-executor. Это упрощает кодовую базу и устраняет устаревшие зависимости, что способствует более чистой и управляемой структуре проекта.

--- coin-executor/pkg/branching/validate.go
+++
@@ -1,7 +0,0 @@
-package branching
-
-// ValidateBranch checks the current branch against ordered branch rules.
-func ValidateBranch(m *Model, branch string) error {
-	_, err := m.Match(branch)
-	return err
-}
